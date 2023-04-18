3394361474d99355ee38793e5d4692aa2d68ad59
Neriya Rosner <neriyarosner@gmail.com>
2023-04-18 16:55:16 +0300

    swiper and announcement bar - design and defer

--- assets/component-announcement-bar.js
+++ assets/component-announcement-bar.js
@@ -1,18 +1,18 @@
 function announcementAnimation() {
-  let announcement = document.querySelector(".announcement-bar-container");
-  let announcementDuration = announcement.dataset.duration * 100;
-  let announcementDelay = announcement.dataset.delay * 1000;
-  let elements = $(".announcement-bar");
-  let index = 0;
-  setInterval(function () {
-    elements.eq(index).fadeOut(announcementDuration, function () {
-      $(this).removeClass("active");
-      index = (index + 1) % elements.length; // creates a loop
-      elements.eq(index).fadeIn(announcementDuration).addClass("active");
-    });
-  }, announcementDelay);
+    let announcement = document.querySelector(".announcement-bar-container");
+    let announcementDuration = announcement.dataset.duration * 100;
+    let announcementDelay = announcement.dataset.delay * 1000;
+    let elements = jQuery(".announcement-bar");
+    let index = 0;
+    setInterval(function () {
+        elements.eq(index).fadeOut(announcementDuration, function () {
+            jQuery(this).removeClass("active");
+            index = (index + 1) % elements.length; // creates a loop
+            elements.eq(index).fadeIn(announcementDuration).addClass("active");
+        });
+    }, announcementDelay);
 }
 
-if ($(window).width() < 990) {
-  announcementAnimation();
+if (jQuery(window).width() < 990) {
+    announcementAnimation();
 }
---
+++ assets/component-swiper.js
@@ -0,0 +1,21 @@
+function swiperAnimation() {
+    const swiper = jQuery(".swiper-container");
+    const swiperDuration = parseInt(swiper.data("duration")) * 100;
+    const swiperDelay = parseInt(swiper.data("delay")) * 1000;
+    const elements = swiper.find(".swiper-slide");
+    let index = 0;
+
+    setInterval(function () {
+        elements.eq(index).fadeOut(swiperDuration, function () {
+            jQuery(this).removeClass("active");
+            index = (index + 1) % elements.length; // creates a loop
+            elements.eq(index).fadeIn(swiperDuration).addClass("active");
+        });
+    }, swiperDelay);
+}
+
+setTimeout(function () {
+    swiperAnimation();
+}, 1000);
+
+
