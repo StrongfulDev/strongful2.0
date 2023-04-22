92e3d86e4a1599a1a5aeafbf3771bfe5fb2b27df
Yahav Honig <yahavhonig@Yahavs-MacBook-Pro.local>
2023-04-22 21:12:34 +0300

    colleciton filter advancement

--- assets/facets.js
+++ assets/facets.js
@@ -284,90 +284,51 @@ customElements.define('facet-remove', FacetRemove);
     }
   }
 
-let x, i, j, l, ll, selElmnt, a, b, c;
-/* Look for any elements with the class "custom-select": */
-x = document.getElementsByClassName(".mobile-facets__sort .select");
-l = x.length;
-for (i = 0; i < l; i++) {
-  console.log(x[i]);
-  selElmnt = x[i].getElementsByTagName("select")[0];
-  ll = selElmnt.length;
-  /* For each element, create a new DIV that will act as the selected item: */
-  a = document.createElement("DIV");
-  a.setAttribute("class", "select-selected");
-  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-  x[i].appendChild(a);
-  /* For each element, create a new DIV that will contain the option list: */
-  b = document.createElement("DIV");
-  b.setAttribute("class", "select-items select-hide");
-  for (j = 1; j < ll; j++) {
-    /* For each option in the original select element,
-    create a new DIV that will act as an option item: */
-    c = document.createElement("DIV");
-    c.innerHTML = selElmnt.options[j].innerHTML;
-    c.addEventListener("click", function(e) {
-        /* When an item is clicked, update the original select box,
-        and the selected item: */
-        let y, i, k, s, h, sl, yl;
-        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-        sl = s.length;
-        h = this.parentNode.previousSibling;
-        for (i = 0; i < sl; i++) {
-          if (s.options[i].innerHTML == this.innerHTML) {
-            s.selectedIndex = i;
-            h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("same-as-selected");
-            yl = y.length;
-            for (k = 0; k < yl; k++) {
-              y[k].removeAttribute("class");
-            }
-            this.setAttribute("class", "same-as-selected");
-            if (s.options[i].innerText.replace(/\s/g, '') == this.innerText.replace(/\s/g, '')) {
-              s.options[i].setAttribute('selected', 'selected');
-              console.log(s.options[i]);
-            } else {
-              s.options[i].removeAttribute('selected');
-            }
-            break;
-          }
-        }
-        h.click();
-    });
-    b.appendChild(c);
-  }
-  x[i].appendChild(b);
-  a.addEventListener("click", function(e) {
-    /* When the select box is clicked, close any other select boxes,
-    and open/close the current select box: */
-    e.stopPropagation();
-    closeAllSelect(this);
-    this.nextSibling.classList.toggle("select-hide");
-    this.previousElementSibling.classList.toggle("active");
-  });
-}
+  // let colorLabels = document.querySelectorAll(".mobile-facets__label");
+  // let colorLabelsArray = Array.from(colorLabels).filter(label => label.htmlFor.includes("color"));
 
-function closeAllSelect(elmnt) {
-  /* A function that will close all select boxes in the document,
-  except the current select box: */
-  let x, y, i, xl, yl, arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  xl = x.length;
-  yl = y.length;
-  for (i = 0; i < yl; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i)
-    } else {
-      y[i].classList.remove("select-arrow-active");
-    }
-  }
-  for (i = 0; i < xl; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
-    }
-  }
-}
+  let colorLabels = document.querySelectorAll(".color-label");
+  let colorRounds = document.querySelectorAll(".color-round");
 
-/* If the user clicks anywhere outside the select box,
-then close all select boxes: */
-document.addEventListener("click", closeAllSelect);
+  for (let i = 0; i < colorLabels.length; i++) {
+      let colorLabel = colorLabels[i];
+      let colorRound = colorRounds[i];
+      switch (colorLabel.innerText) {
+          case "שחור":
+              colorRound.style.backgroundColor = "#000000";
+              break;
+          case "לבן":
+              colorRound.style.backgroundColor = "#ffffff";
+              break;
+          case "אדום":
+              colorRound.style.backgroundColor = "#ff0000";
+              break;
+          case "כחול":
+              colorRound.style.backgroundColor = "#0000ff";
+              break;
+          case "ירוק":
+              colorRound.style.backgroundColor = "#00ff00";
+              break;
+          case "צהוב":
+              colorRound.style.backgroundColor = "#ffff00";
+              break;
+          case "כתום":
+              colorRound.style.backgroundColor = "#ffa500";
+              break;
+          case "סגול":
+              colorRound.style.backgroundColor = "#800080";
+              break;
+          case "ורוד":
+              colorRound.style.backgroundColor = "#ffc0cb";
+              break;
+          case "חום":
+              colorRound.style.backgroundColor = "#a52a2a";
+              break;
+          case "אפור":
+              colorRound.style.backgroundColor = "#808080";
+              break;
+              default:
+                console.log("no color");
+                break;
+      }
+  }
