90cc44c285017118ce21a7fa4d8d5f2e6f9f6e60
Neriya Rosner <neri.coder@gmail.com>
2023-04-24 13:22:45 +0300

    fix click event binding

--- assets/component-variant-size-picker.js
+++ assets/component-variant-size-picker.js
@@ -13,38 +13,37 @@ function quickAdd() {
     element.parents('.variant_selector').find('.add_to_cart').removeAttr("disabled");
   })
 
-  $('.variant_selector.submit_on_click .size_variant_button:not(.disabled-variant-button)').on("click", function (e) {
-    const element = $(this);
-    let myElement = this;
-    $(myElement).find(".size_variant_button_add").hide();
-    $(myElement).find(".size-picker-loading").addClass("block");
-    setTimeout(() => {
-      element.parents("form").find('[type="submit"]').click()
-    }, 100);
-    setTimeout(() => {
-      $(myElement).find(".size_variant_button_add").show();
-      $(myElement).find(".size-picker-loading").removeClass("block");
-    }, 500);
-  });
-
-  $('#MainContent').on("click", ".variant_modal__toggle_button, .variant_modal__toggle_button svg", function (e) {
+  $('#MainContent')
+    .on("click", '.variant_selector.submit_on_click .size_variant_button:not(.disabled-variant-button)', function (e) {
       const element = $(this);
-      const modalId = element.data("modal-id");
-      const modal = $(".variant_modal#" + modalId);
-      const modalOverlay = $(".variant_modal_overlay#" + modalId + "-overlay");
+      element.find(".size_variant_button_add").hide();
+      element.find(".size-picker-loading").addClass("block");
+      setTimeout(() => {
+        element.parents("form").find('[type="submit"]').click()
+      }, 100);
+      setTimeout(() => {
+        element.find(".size_variant_button_add").show();
+        element.find(".size-picker-loading").removeClass("block");
+      }, 500);
+    })
+    .on("click", ".variant_modal__toggle_button, .variant_modal__toggle_button svg", function (e) {
+        const element = $(this);
+        const modalId = element.data("modal-id");
+        const modal = $(".variant_modal#" + modalId);
+        const modalOverlay = $(".variant_modal_overlay#" + modalId + "-overlay");
 
-      modal.show();
-      modalOverlay.show()
-      modalOverlay.on("click", () => {
-        modal.hide()
-        modalOverlay.hide()
-      })
-      modal.find('.quick_add_modal__close').on("click", () => {
-        modal.hide()
-        modalOverlay.hide()
-      })
-    }
-  )
+        modal.show();
+        modalOverlay.show()
+        modalOverlay.on("click", () => {
+          modal.hide()
+          modalOverlay.hide()
+        })
+        modal.find('.quick_add_modal__close').on("click", () => {
+          modal.hide()
+          modalOverlay.hide()
+        })
+      }
+    )
 }
 
 window.onload = () => {
