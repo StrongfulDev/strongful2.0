f54b594b984bc1f181030d4a34338bedd2a32f6a
Yahav Honig <yahavhonig@Yahavs-MacBook-Pro.local>
2023-04-27 15:04:07 +0300

    script to design sorting

--- assets/facets.js
+++ assets/facets.js
@@ -361,25 +361,103 @@ function hideInStockFacet() {
   });
 }
 
+  function designSelect() {
+    let x, i, j, l, ll, selElement, a, b, c;
+    /* Look for any elements with the class "custom-select": */
+    x = document.getElementsByClassName("select");
+    l = x.length;
+    console.log(x)
+    for (i = 0; i < l; i++) {
+      selElement = x[i].querySelector(".select__select");
+      ll = selElement.length;
+      /* For each element, create a new DIV that will act as the selected item: */
+      a = document.createElement("DIV");
+      a.setAttribute("class", "select-selected");
+      a.innerHTML = selElement.options[selElement.selectedIndex].innerHTML;
+      x[i].appendChild(a);
+      /* For each element, create a new DIV that will contain the option list: */
+      b = document.createElement("DIV");
+      b.setAttribute("class", "select-items select-hide");
+      for (j = 1; j < ll; j++) {
+        /* For each option in the original select element,
+				create a new DIV that will act as an option item: */
+        c = document.createElement("DIV");
+        c.innerHTML = selElement.options[j].innerHTML;
+        c.addEventListener("click", function(e) {
+          /* When an item is clicked, update the original select box,
+					and the selected item: */
+          let y, i, k, s, h, sl, yl;
+          s = this.parentNode.parentNode.querySelector(".select__select");
+          sl = s.length;
+          h = this.parentNode.previousSibling;
+          for (i = 0; i < sl; i++) {
+            if (s.options[i].innerHTML == this.innerHTML) {
+              s.selectedIndex = i;
+              h.innerHTML = this.innerHTML;
+              y = this.parentNode.getElementsByClassName("same-as-selected");
+              yl = y.length;
+              for (k = 0; k < yl; k++) {
+                y[k].removeAttribute("class");
+              }
+              this.setAttribute("class", "same-as-selected");
+              break;
+            }
+          }
+          h.click();
+          // Update hidden input element with selected option value
+          let hiddenInput = this.parentNode.parentNode.querySelector('input[type="hidden"]');
+          hiddenInput.value = s.options[s.selectedIndex].value;
+        });
+        b.appendChild(c);
+      }
+      x[i].appendChild(b);
+      a.addEventListener("click", function(e) {
+        /* When the select box is clicked, close any other select boxes,
+				and open/close the current select box: */
+        e.stopPropagation();
+        closeAllSelect(this);
+        this.nextSibling.classList.toggle("select-hide");
+        this.classList.toggle("select-arrow-active");
+      });
+      // Add event listener to original select element to update hidden input element
+      selElement.addEventListener("change", function(e) {
+        let hiddenInput = this.parentNode.querySelector('input[type="hidden"]');
+        hiddenInput.value = this.options[this.selectedIndex].value;
+      });
+    }
+
+    function closeAllSelect(element) {
+      /* A function that will close all select boxes in the document,
+			except the current select box: */
+      let x, y, i, xl, yl, arrNo = [];
+      x = document.getElementsByClassName("select-items");
+      y = document.getElementsByClassName("select-selected");
+      xl = x.length;
+      yl = y.length;
+      for (i = 0; i < yl; i++) {
+        if (element == y[i]) {
+          arrNo.push(i)
+        } else {
+          y[i].classList.remove("select-arrow-active");
+        }
+      }
+      for (i = 0; i < xl; i++) {
+        if (arrNo.indexOf(i)) {
+          x[i].classList.add("select-hide");
+        }
+      }
+    }
+
+
+          /* If the user clicks anywhere outside the select box,
+					then close all select boxes: */
+  document.addEventListener("click", closeAllSelect);
+}
+
 colorRounds(document);
 sizeRounds(document);
 hideInStockFacet();
+designSelect();
 
 // TODO: Insert to class
 document.getElementById('Filter-filter.v.availability-mobile-1').checked = true;
-
-// let customUrl = new URL(window.location.href);
-// customUrl.searchParams.set('filter.v.availability', '1');
-// window.history.pushState({}, '', customUrl);
-//
-// if (customUrl.search === '?filter.v.availability=1') {
-//   $("facet-remove").hide();
-// }
-
-// let activeFilter = document.querySelectorAll('.active-facets__button-inner.button.button--tertiary');
-// for (let i = 0; i < activeFilter.length; i++) {
-//   let filterText = activeFilter[i].innerText;
-//   if (filterText.includes('Availability: In stock')) {
-//     $(activeFilter[i]).closest('facet-remove').hide();
-//   }
-// }
