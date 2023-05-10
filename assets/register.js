a0f316ef19aa537f0f57d69f5cf47e6f00666209
yahavhonig <yahav@strongful.co.il>
2023-05-10 23:36:42 +0300

    finish signup and login basic func and design

--- assets/register.js
+++ assets/register.js
@@ -1,6 +1,23 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
+	let customURL = window.location.href;
+	if (customURL.includes('register')) {
+		$(".multicolumn-list__item").eq(0).removeClass("active");
+		$(".multicolumn-list__item").eq(1).addClass("active");
+		let activeWidth = $(".multicolumn-list__item").eq(1).innerWidth();
+		let position = $(".multicolumn-list__item").eq(1).position();
+		$("#js-border-position").css("width", activeWidth + "px");
+		$("#js-border-position").css("left", position.left + "px");
+	} else {
+		$(".multicolumn-list__item").eq(0).addClass("active");
+		$(".multicolumn-list__item").eq(1).removeClass("active");
+		let activeWidth = $(".multicolumn-list__item").eq(0).innerWidth();
+		let position = $(".multicolumn-list__item").eq(0).position();
+		$("#js-border-position").css("width", activeWidth - 14 + "px");
+		$("#js-border-position").css("left", position.left + 14 + "px");
+	}
+
 	$('.multicolumn-list__item').on('click', function() {
 		$('.multicolumn-list__item').removeClass('active');
 		$(this).addClass('active');
@@ -8,7 +25,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		let position = $(this).position();
 		let id = $(this).attr('id');
 		let lastChar = id.substr(id.length - 1);
-		$(".customer").toggle();
+		$(".customer").toggleClass("hidden");
 		if (lastChar == 2) {
 			$("#js-border-position").css("width", activeWidth + "px");
 			$("#js-border-position").css("left", position.left + "px");
