84ffcc4f88a4d08e831e9eed684613d147f6acdb
yahavhonig <yahav@strongful.co.il>
2023-05-18 13:02:17 +0300

    finish blog design for desktop

---
+++ assets/section-blog-post.js
@@ -0,0 +1,9 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+	let allParagraphs = document.querySelectorAll('.article-template__content p');
+	allParagraphs.forEach((paragraph) => {
+		if (paragraph.innerHTML === '' || paragraph.innerHTML === '&nbsp;') {
+			paragraph.remove();
+		}
+	});
+});
