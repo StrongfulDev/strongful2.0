bfe6de5a060b36498f8b1d68658f96445f60e2e3
yahavhonig <yahav@strongful.co.il>
2023-06-01 20:46:56 +0300

    fix header

--- assets/component-announcement-bar.js
+++ assets/component-announcement-bar.js
@@ -6,13 +6,13 @@
 		    let announcementDelay = announcement.dataset.delay * 1000;
 		    let elements = $(".announcement-bar");
 		    let index = 0;
-		    // setInterval(function () {
-			  //   elements.eq(index).fadeOut(announcementDuration, function () {
-				//     this.classList.remove('active');
-				//     index = (index + 1) % elements.length; // creates a loop
-				//     elements.eq(index).fadeIn(announcementDuration).addClass("active");
-			  //   });
-		    // }, announcementDelay);
+		    setInterval(function () {
+			    elements.eq(index).fadeOut(announcementDuration, function () {
+				    this.classList.remove('active');
+				    index = (index + 1) % elements.length; // creates a loop
+				    elements.eq(index).fadeIn(announcementDuration).addClass("active");
+			    });
+		    }, announcementDelay);
 	    }
 
 	    if (window.screen.width < 990) {
