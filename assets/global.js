4a329e7a8f3eeec0459ed702602e7b02600cc0e6
yahavhonig <yahav@strongful.co.il>
2023-06-28 11:03:47 +0300

    increase cart footer z-index

--- assets/global.js
+++ assets/global.js
@@ -1267,18 +1267,6 @@ window.addEventListener('DOMContentLoaded', function() {
 
 });
 
-function getViewportHeight() {
-	// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-		let vh = window.innerHeight * 0.01;
-	// Then we set the value in the --vh custom property to the root of the document
-		document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-
-// We listen to the resize event
-window.addEventListener('resize', getViewportHeight);
-
-getViewportHeight();
-
 // check the previous window url
 function checkUrl() {
 	let previousUrl = sessionStorage.getItem('previousUrl');
