8898b287615e80ed54af1e61ba880eb531d54c5e
yahavhonig <yahav@strongful.co.il>
2023-07-30 19:57:33 +0300

    more designs

--- assets/package-protection-extension.js
+++ assets/package-protection-extension.js
@@ -2,21 +2,21 @@ const extendPackageProtection = () => {
 	const itemsContainer = document.querySelector(`[data-order-summary-section="line-items"]`);
 
 	subscribe('PackageProtection/addToCard', (data) => {
-		console.log('packageProtection/addToCart', data);
+		window.location.reload();
 		const templateElement = itemsContainer.querySelector(".product").cloneNode(true);
-		templateElement.querySelector(".product-thumbnail__image").src = data.product.image;
-		templateElement.querySelector(".product__description__name").innerText = data.product.name;
+		console.log(data);
+		console.log(data.product);
+		templateElement.querySelector(".product-thumbnail__image").src = data.product.image.replace("https:", "");
+		templateElement.querySelector(".product__description__name").innerText = data.product.title;
 		templateElement.querySelector(".product__description__variant").remove();
 		templateElement.dataset.variantId = data.product.id.toString();
-		templateElement.querySelector(".product__price .order-summary__emphasis").innerText = data.product.price;
-		// templateElement.querySelectorAll(".product-thumbnail__quantity, .product__quantity:has( :not(.visually-hidden))").remove()
+		templateElement.querySelector(".product__price .order-summary__emphasis").innerText = "₪" + parseFloat(data.product.price / 100);
 		itemsContainer.appendChild(templateElement);
 	})
 
 	subscribe('PackageProtection/removeFromCart', (data) => {
-		console.log('packageProtection/removeFromCart', data);
 		itemsContainer.querySelector(`[data-variant-id="${data.product.id}"]`).remove();
-		// window.location = window.location;
+		window.location.reload();
 	})
 }
 
