b12605afa868de59eee6f766d6025699d5d64181
yahavhonig <yahav@strongful.co.il>
2023-08-17 12:50:15 +0300

    finish page

--- assets/component-accordion.js
+++ assets/component-accordion.js
@@ -1,19 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
 	if (document.getElementById("search-input") !== null) {
 		document.getElementById('search-input').addEventListener('input', function() {
+
 			const query = this.value.toLowerCase();
 			const faqContainers = document.querySelectorAll('.accordion details');
+			const noResults = document.querySelector('.search-input__no-results');
+			let hiddenElementsCount = 0;
+			let totalElementsLength = faqContainers.length;
 
 			faqContainers.forEach(container => {
 				const questionText = container.querySelector('.accordion__title.h4').textContent.toLowerCase();
 				const answerText = container.querySelector('.accordion .accordion__content.rte').textContent.toLowerCase();
 
 				if (questionText.includes(query) || answerText.includes(query)) {
-					container.style.display = 'block';
+					container.classList.remove('hidden');
+					hiddenElementsCount--;
 				} else {
-					container.style.display = 'none';
+					container.classList.add('hidden');
+					hiddenElementsCount++;
 				}
 			});
+
+			if (hiddenElementsCount === totalElementsLength) {
+				noResults.classList.remove('hidden');
+			} else {
+				noResults.classList.add('hidden');
+			}
 		});
 	}
 });
