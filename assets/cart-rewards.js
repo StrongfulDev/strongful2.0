2961b32728459459cc9c7616cd430cf350ec5f95
yahavhonig <yahav@strongful.co.il>
2023-09-13 14:02:46 +0300

    fix cart rewards

--- assets/cart-rewards.js
+++ assets/cart-rewards.js
@@ -43,16 +43,18 @@ class CartRewards {
 			const isRewardInCart = this.cartHasReward(rule);
 			const rewardItem = this.getRewardItemByRule(rule);
 
-			if (rule.condition.type == 'CustomerTags') {
-				const ruleVariantId = rule.reward.variantId;
+			if (rule.condition.type === 'CustomerTags') {
+				const ruleVariantId = parseInt(rule.reward.variantId);
 				const loyaltyText = rule.reward.loyaltyText;
 				const frontCartItems = document.querySelectorAll('.cart-item');
 				frontCartItems.forEach((item) => {
 					const itemId = parseInt(item.dataset.id);
+					console.log(itemId, ruleVariantId);
 					const loyaltyDiscount = $(item).find('.loyalty-reward-discount-text');
 
 					if (itemId === ruleVariantId) {
-						$(loyaltyDiscount).removeClass('hidden').text(loyaltyText);
+						$(loyaltyDiscount).removeClass('hidden');
+						$(loyaltyDiscount).html(loyaltyText);
 					}
 				});
 			}
@@ -124,8 +126,8 @@ class CartRewards {
 			isConditionMet = (isRightQuantity || isRightQuantity === null) && (isAmountGreaterThan || isAmountLessThan);
 		} else if (rule.condition.type === "CustomerTags") {
 			if (customerTags.includes(rule.customer_tags) && this.cartTotalValue > rule.condition.value) {
-				// isConditionMet = (isRightQuantity || isRightQuantity === null) && (isAmountGreaterThan || isAmountLessThan);
-				isConditionMet = false;
+				isConditionMet = (isRightQuantity || isRightQuantity === null) && (isAmountGreaterThan || isAmountLessThan);
+				// isConditionMet = false;
 			} else {
 				isConditionMet = false;
 			}
@@ -245,10 +247,10 @@ class CartRewards {
 
 	trackProgress() {
 		const progressPercentage = (this.cartTotalValue / this.allRewardsAmount) * 100;
-			$('.progress-value').animate({
-				width: `${progressPercentage}%`
-			})
-		}
+		$('.progress-value').animate({
+			width: `${progressPercentage}%`
+		})
+	}
 
 	toggleMessage(isConditionMet, rule, ruleIndex) {
 
@@ -327,10 +329,10 @@ class CartRewards {
 			return true;
 		}
 
-			const product = this.cart.items.find(item => item.id === parseInt(productIdsInCart[0]));
-			if (product) {
-				return product.quantity === acceptableQuantity;
-			}
+		const product = this.cart.items.find(item => item.id === parseInt(productIdsInCart[0]));
+		if (product) {
+			return product.quantity === acceptableQuantity;
+		}
 	}
 
 	productsExistInCart(productIds) {
