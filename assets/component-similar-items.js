6704666b81d19b008f8378388e25a156405efb69
yahavhonig <yahav@strongful.co.il>
2023-09-28 16:43:07 +0300

    fix similiar typo to similar

--- assets/component-similar-items.js
+++ assets/component-similar-items.js
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	const productVendorText = document.querySelector('.product__title').dataset.productColor;
 	similarVendors.forEach((vendor) => {
 		if (vendor.textContent === productVendorText) {
-			vendor.closest(".similiar-child").classList.add("first-similar-item");
+			vendor.closest(".similar-child").classList.add("first-similar-item");
 		}
 	});
 });
