22f3ee6fd2b67c1ce98b91dfbd092614609d834d
yahavhonig <yahav@strongful.co.il>
2023-11-14 10:52:17 +0200

    direction fixes

--- assets/component-model-switcher.js
+++ assets/component-model-switcher.js
@@ -1,80 +1,82 @@
 window.addEventListener('DOMContentLoaded', function(e) {
 
-	$(".model-switcher").removeClass("hidden");
+	setTimeout(function() {
+		$(".model-switcher").removeClass("hidden");
 
-	const arrowIcon = document.querySelector(".model-switch-button .icon-caret");
-	hideUnAvailableModels();
+		const arrowIcon = document.querySelector(".model-switch-button .icon-caret");
+		hideUnAvailableModels();
 
 		let defaultSize = $('input[name="מידה"]:checked').val();
 		updateVariantRadios(defaultSize);
 		changeModel(defaultSize);
 		updateModelSwitch(defaultSize);
 
-	$('.model-switcher__item input').change(function () {
-		let modelSize = $(this).val();
-		if (modelSize) {
+		$('.model-switcher__item input').change(function () {
+			let modelSize = $(this).val();
+			if (modelSize) {
 				updateVariantRadios(modelSize);
-			changeModel(modelSize);
+				changeModel(modelSize);
 				updateModelSwitch(modelSize);
-			$(this).parents().addClass("inactive");
-			$(this).parents().siblings().removeClass("inactive");
-		}
-	});
+				$(this).parents().addClass("inactive");
+				$(this).parents().siblings().removeClass("inactive");
+			}
+		});
 
 		// $('variant-radios input[name="Size"]').on('change', function (e) {
 		// 	const modelSize = $(this).val();
-	// 	changeModel(modelSize);
+		// 	changeModel(modelSize);
 		// 	updateModelSwitch(modelSize);
 		// 	if (window.innerWidth < 990) {
 		// 		showLowStock(this);
-	// }
+		// }
 		// });
 
-	function showLowStock(element) {
-		let lowStockText = $(element).siblings(".product__inventory");
-		let otherLowStocks = $(element).parents().find(".product__inventory").not(lowStockText);
-		otherLowStocks.hide();
-		if (lowStockText.text() !== "") {
-			lowStockText.show();
-		}
-	}
-
-	function hideUnAvailableModels() {
-		const availableSizes = getAvailableSizes();
-
-		if (availableSizes.length === 0) {
-			$('.model-switcher').hide();
+		function showLowStock(element) {
+			let lowStockText = $(element).siblings(".product__inventory");
+			let otherLowStocks = $(element).parents().find(".product__inventory").not(lowStockText);
+			otherLowStocks.hide();
+			if (lowStockText.text() !== "") {
+				lowStockText.show();
+			}
 		}
 
-		$(".model-switcher__item").filter((function () {
-			const size = $(this).data('model-size');
-			return !availableSizes.includes(size);
-		})).hide();
-	}
+		function hideUnAvailableModels() {
+			const availableSizes = getAvailableSizes();
 
-	function getAvailableSizes() {
-		const sizes = new Set()
-		$('.product__media-item:not([data-model-size=""])').each(function () {
-			sizes.add($(this).data('model-size'));
-		});
+			if (availableSizes.length === 0) {
+				$('.model-switcher').hide();
+			}
 
-		return Array.from(sizes);
-	}
+			$(".model-switcher__item").filter((function () {
+				const size = $(this).data('model-size');
+				return !availableSizes.includes(size);
+			})).hide();
+		}
 
-	function changeModel(modelSize) {
+		function getAvailableSizes() {
+			const sizes = new Set()
+			$('.product__media-item:not([data-model-size=""])').each(function () {
+				sizes.add($(this).data('model-size'));
+			});
+
+			return Array.from(sizes);
+		}
+
+		function changeModel(modelSize) {
 			const productMediaList = $('.product__media-list');
-		const sizeTable = $('.div-block-460');
+			const sizeTable = $('.div-block-460');
 			const selectedColorValue = $('input[name="צבע"]:checked').val();
+			console.log(selectedColorValue)
 
 			const colorImagesToHide = $(document).find(`.product__media-item:not([data-alt="${selectedColorValue}"])`);
 			const sizeImagesToHide = $(document).find(`.product__media-item:not([data-model-size="${modelSize}"])`);
 			const imagesToShow = productMediaList.find(`.product__media-item[data-model-size="${modelSize}"][data-alt="${selectedColorValue}"]`);
 
-		const divsToHide = sizeTable.find(`span:not([data-model-size="${modelSize}"])`);
-		const divsToShow = sizeTable.find(`span[data-model-size="${modelSize}"]`);
+			const divsToHide = sizeTable.find(`span:not([data-model-size="${modelSize}"])`);
+			const divsToShow = sizeTable.find(`span[data-model-size="${modelSize}"]`);
 
-		const modelWearsSizeParagraphsToShow = $(document).find(`.model-wears-size[data-model="${modelSize}"]`);
-		const modelWearsSizeParagraphsToHide = $(document).find(`.model-wears-size:not([data-model="${modelSize}"])`);
+			const modelWearsSizeParagraphsToShow = $(document).find(`.model-wears-size[data-model="${modelSize}"]`);
+			const modelWearsSizeParagraphsToHide = $(document).find(`.model-wears-size:not([data-model="${modelSize}"])`);
 
 			let progressBar = $('.slider-component-progress-bar');
 
@@ -87,42 +89,43 @@ window.addEventListener('DOMContentLoaded', function(e) {
 			modelWearsSizeParagraphsToHide.addClass('hidden');
 			progressBar.css('width', `calc(100% / ${imagesToShow.length})`);
 
-		$(".model-switcher__list").addClass("hidden");
-		$(".model-switcher-overlay").addClass("hidden");
-		$(arrowIcon).removeClass('rotate-arrow');
+			$(".model-switcher__list").addClass("hidden");
+			$(".model-switcher-overlay").addClass("hidden");
+			$(arrowIcon).removeClass('rotate-arrow');
 
-		const model = window.models[modelSize];
+			const model = window.models[modelSize];
 
-	}
-
-	function updateVariantRadios(modelSize) {
-			const button = $(`variant-radios input[name="מידה"][value="${modelSize}"]`)
-		button.click();
-	}
-
-	function updateModelSwitch(modelSize) {
-		const inputs = $('.model-switcher__item input')
-		inputs
-			.removeAttr('checked')
-			.filter(`[value="${modelSize}"]`)
-			.attr('checked', true).prop('checked', true);
-	}
-
-	$(".model-switcher-overlay").click(function() {
-		$(this).addClass("hidden");
-		$(".model-switcher__list").addClass("hidden");
-		$(arrowIcon).removeClass('rotate-arrow');
-	});
-
-	$(".model-switch-button").click(function(e) {
-
-		$(".model-switcher__list").toggleClass("hidden");
-
-		$(arrowIcon).toggleClass('rotate-arrow');
-
-		if ($(window).width() < 990) {
-			$(".model-switcher-overlay").toggleClass("hidden");
 		}
-	});
+
+		function updateVariantRadios(modelSize) {
+			const button = $(`variant-radios input[name="מידה"][value="${modelSize}"]`)
+			button.click();
+		}
+
+		function updateModelSwitch(modelSize) {
+			const inputs = $('.model-switcher__item input')
+			inputs
+				.removeAttr('checked')
+				.filter(`[value="${modelSize}"]`)
+				.attr('checked', true).prop('checked', true);
+		}
+
+		$(".model-switcher-overlay").click(function() {
+			$(this).addClass("hidden");
+			$(".model-switcher__list").addClass("hidden");
+			$(arrowIcon).removeClass('rotate-arrow');
+		});
+
+		$(".model-switch-button").click(function(e) {
+
+			$(".model-switcher__list").toggleClass("hidden");
+
+			$(arrowIcon).toggleClass('rotate-arrow');
+
+			if ($(window).width() < 990) {
+				$(".model-switcher-overlay").toggleClass("hidden");
+			}
+		});
+	}, 500);
 
 });
