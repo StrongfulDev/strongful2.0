a5847d3e8c1f0a9ef68471e5d9c60e021fca9bfd
yahavhonig <yahav@strongful.co.il>
2023-11-23 20:14:59 +0200

    countdown

--- assets/component-countdown.js
+++ assets/component-countdown.js
@@ -6,34 +6,34 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	if (document.getElementById('tiles') != null) {
 
-		// getCountdown();
-		//
-		// setInterval(() => { getCountdown(); }, 1000);
-		//
-		// function getCountdown() {
-		//
-		// 	// find the amount of "seconds" between now and target
-		// 	let current_date = new Date().getTime();
-		// 	let seconds_left = (target_date - current_date) / 1000;
-		//
-		// 	days = pad(parseInt(seconds_left / 86400));
-		// 	seconds_left = seconds_left % 86400;
-		//
-		// 	hours = pad(parseInt(seconds_left / 3600));
-		// 	seconds_left = seconds_left % 3600;
-		//
-		// 	minutes = pad(parseInt(seconds_left / 60));
-		// 	seconds = pad(parseInt(seconds_left % 60));
-		//
-		// 	$('.tiles-time-days').html(days);
-		// 	$('.tiles-time-hours').html(hours + '<span class="tiles-time-hours-small"> :</span>');
-		// 	$('.tiles-time-minutes').html(minutes + '<span class="tiles-time-minutes-small"> :</span>');
-		// 	$('.tiles-time-seconds').html(seconds);
-		// }
-		//
-		// function pad(n) {
-		// 	return (n < 10 ? '0' : '') + n;
-		// }
+		getCountdown();
+
+		setInterval(() => { getCountdown(); }, 1000);
+
+		function getCountdown() {
+
+			// find the amount of "seconds" between now and target
+			let current_date = new Date().getTime();
+			let seconds_left = (target_date - current_date) / 1000;
+
+			days = pad(parseInt(seconds_left / 86400));
+			seconds_left = seconds_left % 86400;
+
+			hours = pad(parseInt(seconds_left / 3600));
+			seconds_left = seconds_left % 3600;
+
+			minutes = pad(parseInt(seconds_left / 60));
+			seconds = pad(parseInt(seconds_left % 60));
+
+			$('.tiles-time-days').html(days);
+			$('.tiles-time-hours').html(hours + '<span class="tiles-time-hours-small"> :</span>');
+			$('.tiles-time-minutes').html(minutes + '<span class="tiles-time-minutes-small"> :</span>');
+			$('.tiles-time-seconds').html(seconds);
+		}
+
+		function pad(n) {
+			return (n < 10 ? '0' : '') + n;
+		}
 	}
 
 	const startTime = new Date(data).getTime(); // GMT +0300
@@ -41,38 +41,38 @@ document.addEventListener('DOMContentLoaded', function() {
 	const totalCount = 1000;
 	const fastPhaseDuration = 3 * 60 * 60 * 1000; // 3 hours in milliseconds
 
-	function getCurrentCount() {
-		const now = Date.now();
-		if (now < startTime) {
-			return totalCount; // Countdown hasn't started yet
-		} else if (now > endTime) {
-			return 0; // Countdown has ended
-		} else {
-			const elapsed = now - startTime;
+	// function getCurrentCount() {
+	// 	const now = Date.now();
+	// 	if (now < startTime) {
+	// 		return totalCount; // Countdown hasn't started yet
+	// 	} else if (now > endTime) {
+	// 		return 0; // Countdown has ended
+	// 	} else {
+	// 		const elapsed = now - startTime;
+	//
+	// 		if (elapsed <= fastPhaseDuration) {
+	// 			// First 3 hours - from 1000 to 500
+	// 			return Math.round(1000 - (elapsed / fastPhaseDuration) * 500);
+	// 		} else {
+	// 			// After first 3 hours - from 500 to 0 over the remaining 21 hours
+	// 			const normalPhaseElapsed = elapsed - fastPhaseDuration;
+	// 			const normalPhaseDuration = endTime - startTime - fastPhaseDuration;
+	// 			return Math.round(500 - (normalPhaseElapsed / normalPhaseDuration) * 500);
+	// 		}
+	// 	}
+	// }
 
-			if (elapsed <= fastPhaseDuration) {
-				// First 3 hours - from 1000 to 500
-				return Math.round(1000 - (elapsed / fastPhaseDuration) * 500);
-			} else {
-				// After first 3 hours - from 500 to 0 over the remaining 21 hours
-				const normalPhaseElapsed = elapsed - fastPhaseDuration;
-				const normalPhaseDuration = endTime - startTime - fastPhaseDuration;
-				return Math.round(500 - (normalPhaseElapsed / normalPhaseDuration) * 500);
-			}
-		}
-	}
+	// function updateCounter() {
+	// 	const count = getCurrentCount();
+	// 	$("#countdownDisplay").text(count + (count === 1 ? " ORDER LEFT" : " ORDERS LEFT"));
+	//
+	// 	if (count > 0) {
+	// 		setTimeout(updateCounter, 1000); // Update every second
+	// 	} else {
+	// 		$("#countdownDisplay").text("PRE ORDER OVER");
+	// 	}
+	// }
 
-	function updateCounter() {
-		const count = getCurrentCount();
-		$("#countdownDisplay").text(count + (count === 1 ? " ORDER LEFT" : " ORDERS LEFT"));
-
-		if (count > 0) {
-			setTimeout(updateCounter, 1000); // Update every second
-		} else {
-			$("#countdownDisplay").text("PRE ORDER OVER");
-		}
-	}
-
-	updateCounter();
+	// updateCounter();
 
 });
