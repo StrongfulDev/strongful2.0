bfaa115504222d9cb885dfde2540e76fbd7bc84e
yahavhonig <yahav@strongful.co.il>
2024-01-07 18:50:03 +0200

    add loyalty js

--- assets/loyalty.js
+++ assets/loyalty.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-	alert();
+	const clickToSubmit = document.createElement('div');
+	clickToSubmit.classList.add('click-to-submit');
+	clickToSubmit.innerHTML = 'לחצי למימוש';
+	const elementsToAppend = document.querySelectorAll('.yotpo-tile:not(.yotpo-tile-completed)');
+	console.log(elementsToAppend);
 
 });
