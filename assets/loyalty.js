cd71c107aae5bbeeb341920cdfec406d344b0e23
yahavhonig <yahav@strongful.co.il>
2024-01-07 18:57:49 +0200

    make buttons appear only on mobile

--- assets/loyalty.js
+++ assets/loyalty.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 		const clickToSubmit = document.createElement('div');
 		clickToSubmit.classList.add('click-to-submit');
 		clickToSubmit.innerHTML = 'לחצי למימוש';
-		const elementsToAppend = document.querySelectorAll('.yotpo-campaign-switcher-wrapper .yotpo-tile:not(.yotpo-tile-completed)');
+		const elementsToAppend = document.querySelectorAll('.yotpo-campaign-switcher-wrapper .yotpo-tile.yotpo-is-mobile:not(.yotpo-tile-completed)');
 
 		elementsToAppend.forEach((element) => {
 			element.appendChild(clickToSubmit.cloneNode(true));
