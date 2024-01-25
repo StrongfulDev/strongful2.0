a1803588de95aff2c9101b2e6a437a072c647d9c
yahavhonig <yahav@strongful.co.il>
2024-01-25 11:11:44 +0200

    fix data layer js

--- assets/register_data_layer.js
+++ assets/register_data_layer.js
@@ -2,11 +2,14 @@
 const customForm = document.querySelector('#create_customer_custom_form');
 
 console.log(customForm);
-customForm.addEventListener('submit', (e) => {
-	e.preventDefault();
 
-	window.dataLayer = window.dataLayer || [];
-	window.dataLayer.push({
-		'event': 'new_club_member'
+if (customForm !== null) {
+	customForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		window.dataLayer = window.dataLayer || [];
+		window.dataLayer.push({
+			'event': 'new_club_member'
+		});
 	});
-});
+}
