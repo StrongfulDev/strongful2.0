f118f29ad271297805825a936d25a54637d5c990
yahavhonig <yahav@strongful.co.il>
2024-03-26 13:02:37 +0200

    change const to let in register data layer

--- assets/register_data_layer.js
+++ assets/register_data_layer.js
@@ -1,11 +1,13 @@
 
 let customForm = document.querySelector('#create_customer_custom_form');
 
-customForm.addEventListener('submit', (e) => {
-	e.preventDefault();
+if (customForm !== null) {
+	customForm.addEventListener('submit', (e) => {
+		e.preventDefault();
 
-	window.dataLayer = window.dataLayer || [];
-	window.dataLayer.push({
-		'event': 'new_club_member'
+		window.dataLayer = window.dataLayer || [];
+		window.dataLayer.push({
+			'event': 'new_club_member'
+		});
 	});
-});
+}
