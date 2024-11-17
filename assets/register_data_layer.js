ebf8d295aa8de087f18422fb7b5451b8b846d842
ishay-dev <ishay@strongful.co.il>
2024-11-17 14:56:41 +0200

    GTM back to normal

--- assets/register_data_layer.js
+++ assets/register_data_layer.js
@@ -4,18 +4,12 @@ let customForm =
 
 if (customForm !== null) {
 	customForm.addEventListener("submit", (e) => {
-		// Prevent form submission only if needed for async actions (like dataLayer push)
 		e.preventDefault();
 
-		// Ensure data is pushed to dataLayer first
 		window.dataLayer = window.dataLayer || [];
 		window.dataLayer.push({
 			event: "new_club_member",
 		});
-
-		// Submit the form after pushing dataLayer
-		setTimeout(() => {
-			customForm.submit(); // Manually submit the form after the dataLayer push
-		}, 100); // Small delay to ensure data is pushed before form submission
+		customForm.submit();
 	});
 }
