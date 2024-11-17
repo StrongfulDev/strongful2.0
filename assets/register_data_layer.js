d01572e23d9977348bae2a12d4959dbe38ebd0c0
ishay-dev <ishay@strongful.co.il>
2024-11-17 14:52:15 +0200

    edit gtm

--- assets/register_data_layer.js
+++ assets/register_data_layer.js
@@ -1,14 +1,21 @@
-
-let customForm = document.querySelector('#create_customer_custom_form') || document.querySelector('#create_customer_custom_loyalty_form');
+let customForm =
+	document.querySelector("#create_customer_custom_form") ||
+	document.querySelector("#create_customer_custom_loyalty_form");
 
 if (customForm !== null) {
-	customForm.addEventListener('submit', (e) => {
+	customForm.addEventListener("submit", (e) => {
+		// Prevent form submission only if needed for async actions (like dataLayer push)
 		e.preventDefault();
 
+		// Ensure data is pushed to dataLayer first
 		window.dataLayer = window.dataLayer || [];
 		window.dataLayer.push({
-			'event': 'new_club_member'
+			event: "new_club_member",
 		});
-		customForm.submit();
+
+		// Submit the form after pushing dataLayer
+		setTimeout(() => {
+			customForm.submit(); // Manually submit the form after the dataLayer push
+		}, 100); // Small delay to ensure data is pushed before form submission
 	});
 }
