8f72bf12043b9bd35a505bdc91418ddf4cfc0be0
StrongfulDev <105498888+StrongfulDev@users.noreply.github.com>
2025-01-09 11:52:25 +0200

    back to OG slide

--- assets/slider.js
+++ assets/slider.js
@@ -20,6 +20,27 @@ class SliderComponent extends HTMLElement {
 		this.nextButton.addEventListener("click", this.onButtonClick.bind(this));
 	}
 
+	cumulativeOffset(element) {
+		return {
+			top: element.offsetTop,
+			left: element.offsetLeft,
+		};
+
+		let top = 0;
+		let left = 0;
+
+		do {
+			top += element.offsetTop || 0;
+			left += element.offsetLeft || 0;
+			element = element.offsetParent;
+		} while (element);
+
+		return {
+			top: top,
+			left: left,
+		};
+	}
+
 	initPages() {
 		this.sliderItemsToShow = Array.from(this.sliderItems).filter((element) => element.clientWidth > 0);
 		if (this.sliderItemsToShow.length < 2) return;
@@ -32,36 +53,25 @@ class SliderComponent extends HTMLElement {
 		this.update();
 	}
 
-	cumulativeOffset(element) {
-		let top = 0,
-			left = 0;
-
-		do {
-			top += element.offsetTop || 0;
-			left += element.offsetLeft || 0;
-			element = element.offsetParent;
-		} while (element);
-
-		return { top, left };
+	resetPages() {
+		this.sliderItems = this.querySelectorAll('[id^="Slide-"]');
+		this.initPages();
 	}
 
 	update() {
+		// Temporarily prevents unneeded updates resulting from variant changes
+		// This should be refactored as part of https://github.com/Shopify/dawn/issues/2057
 		if (!this.slider || !this.nextButton) return;
 
 		const previousPage = this.currentPage;
 		this.currentPage = Math.round(this.slider.scrollLeft / this.sliderItemOffset) + 1;
-
-		// Highlight correct dot
-		const dots = this.querySelectorAll(".slider-dot");
-		dots.forEach((dot) => dot.classList.remove("active"));
-		if (dots[this.currentPage - 1]) dots[this.currentPage - 1].classList.add("active");
 
 		if (this.currentPageElement && this.pageTotalElement) {
 			this.currentPageElement.textContent = this.currentPage;
 			this.pageTotalElement.textContent = this.totalPages;
 		}
 
-		if (this.currentPage !== previousPage) {
+		if (this.currentPage != previousPage) {
 			this.dispatchEvent(
 				new CustomEvent("slideChanged", {
 					detail: {
@@ -72,18 +82,18 @@ class SliderComponent extends HTMLElement {
 			);
 		}
 
-		// Button states
-		if (!this.enableSliderLooping) {
-			if (this.isSlideVisible(this.sliderItemsToShow[0]) && this.slider.scrollLeft === 0) {
-				this.prevButton.setAttribute("disabled", "disabled");
-			} else {
-				this.prevButton.removeAttribute("disabled");
-			}
-			if (this.isSlideVisible(this.sliderItemsToShow[this.sliderItemsToShow.length - 1])) {
-				this.nextButton.setAttribute("disabled", "disabled");
-			} else {
-				this.nextButton.removeAttribute("disabled");
-			}
+		if (this.enableSliderLooping) return;
+
+		if (this.isSlideVisible(this.sliderItemsToShow[0]) && this.slider.scrollLeft === 0) {
+			this.prevButton.setAttribute("disabled", "disabled");
+		} else {
+			this.prevButton.removeAttribute("disabled");
+		}
+
+		if (this.isSlideVisible(this.sliderItemsToShow[this.sliderItemsToShow.length - 1])) {
+			this.nextButton.setAttribute("disabled", "disabled");
+		} else {
+			this.nextButton.removeAttribute("disabled");
 		}
 	}
 
@@ -97,6 +107,7 @@ class SliderComponent extends HTMLElement {
 
 	onButtonClick(event) {
 		event.preventDefault();
+		console.log(this);
 		const step = event.currentTarget.dataset.step || 1;
 		this.slideScrollPosition =
 			event.currentTarget.name === "next"
@@ -105,7 +116,6 @@ class SliderComponent extends HTMLElement {
 		this.slider.scrollTo({
 			left: this.slideScrollPosition,
 		});
-		this.update(); // Call update to refresh the active dot
 	}
 }
 
