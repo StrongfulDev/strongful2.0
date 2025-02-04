4c7ca3ac403a27f944b59870b16273e751b5fc45
StrongfulDev <105498888+StrongfulDev@users.noreply.github.com>
2025-02-04 11:43:11 +0200

    Revert "warnings of product form and component-variant-picker fixed ."
    This reverts commit 6ce8de5ce99e727b9db7186cf107c8676aa7d23f.

--- assets/product-form.js
+++ assets/product-form.js
@@ -1,21 +1,16 @@
 if (!customElements.get('product-form')) {
-    let formCounter = 0;
-
     customElements.define('product-form', class ProductForm extends HTMLElement {
         constructor() {
             super();
 
-            this.form = this.querySelector("form");
-						this.form.querySelector("[name=id]").disabled = false;
-						this.form.addEventListener("submit", this.onSubmitHandler.bind(this));
-						this.cart = document.querySelector("cart-notification") || document.querySelector("cart-drawer");
-						this.submitButton = this.querySelector('[type="submit"]');
-						if (document.querySelector("cart-drawer")) this.submitButton.setAttribute("aria-haspopup", "dialog");
+            this.form = this.querySelector('form');
+            this.form.querySelector('[name=id]').disabled = false;
+            this.form.addEventListener('submit', this.onSubmitHandler.bind(this));
+            this.cart = document.querySelector('cart-notification') || document.querySelector('cart-drawer');
+            this.submitButton = this.querySelector('[type="submit"]');
+            if (document.querySelector('cart-drawer')) this.submitButton.setAttribute('aria-haspopup', 'dialog');
 
-						this.hideErrors = this.dataset.hideErrors === "true";
-
-						// Ensure the form has a unique id
-						this.form.id = `product_form_${Date.now()}_${formCounter++}`;
+            this.hideErrors = this.dataset.hideErrors === 'true';
         }
 
         onSubmitHandler(evt) {
