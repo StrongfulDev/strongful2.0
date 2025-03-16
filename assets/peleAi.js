c22bc1303db3cea787c0abde88bb4dc605ae4959
StrongfulDev <105498888+StrongfulDev@users.noreply.github.com>
2025-03-16 11:53:59 +0200

    try to make everything work

--- assets/peleAi.js
+++ assets/peleAi.js
@@ -1,9 +1,1 @@
-import PeleAI from "https://cdn.pele-ai.com/pele-ai.js";
-
-const language = "he"; //can be "he" (Hebrew) or "en" (English), if not provided English will act as default
-const customerId = "customer-id"; //Provided by Pele-AI, don't change!
-
-PeleAI.initialize({
-	customerId,
-	language,
-});
+import "https://cdn.pele-ai.com/pele-ai.js";
