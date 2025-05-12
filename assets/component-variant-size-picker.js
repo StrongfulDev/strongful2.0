311f1ab0db79f733cb979bcb0f61182c7c716962
StrongfulDev <105498888+StrongfulDev@users.noreply.github.com>
2025-05-12 23:26:36 +0300

    dangot

--- assets/component-variant-size-picker.js
+++ assets/component-variant-size-picker.js
@@ -1,60 +1,54 @@
-window.addEventListener('DOMContentLoaded', function(event) {
-	$('.variant_selector').each(function (index, element) {
+window.addEventListener("DOMContentLoaded", function (event) {
+	$(".variant_selector").each(function (index, element) {
 		element = $(element);
 		if (element.find('input[checked="checked"]').length === 0) {
 			// element.find('.add_to_cart').attr('disabled', 'disabled');
 		}
-	})
+	});
 
-	$('.product_size_picker__button.default_input').parents('.variant_selector').find('.add_to_cart').removeAttr("disabled");
+	$(".product_size_picker__button.default_input")
+		.parents(".variant_selector")
+		.find(".add_to_cart")
+		.removeAttr("disabled");
 
 	$('.variant_selector input[type="radio"]').on("change", function (e) {
 		// $(this).parents('.variant_selector').find('.add_to_cart').removeAttr("disabled");
-	})
+	});
 
 	function closeModal(element, elementOverlay) {
 		element.removeClass("active");
 		setTimeout(() => {
 			element.hide();
 		}, 500);
-		element.parents("section").removeClass('high-z-index');
-		elementOverlay.hide()
+		element.parents("section").removeClass("high-z-index");
+		elementOverlay.hide();
 	}
 
-	$("body").on("change", function (e) {
-        
-        if (e.target.matches('.variant_selector.submit_on_click .size_variant_button:not(.disabled-variant-button) [type="radio"]')) {
-          let $thisOrigin = $(e.target);
-          let $this;
-          if ( window.innerWidth < 1024 ) {
-            $this =  $(e.target).closest('.grid__item').find(`.variant_modal .size_variant_button .product_size_picker__button[value="${e.target.getAttribute('value')}"]`);
-          } else {
-            $this = $(e.target);
-          }
-          
-          $this = $this.closest(".size_variant_button");
-  			let modal = $(".variant_modal") || $(".variant_selector");
-  			let modalOverlay = $(".variant_modal_overlay");
-  				$this.find(".size_variant_button_add").hide();
-  				$this.find(".loading-overlay").css("display", "flex");
-  				$this.siblings().find(".loading-overlay").css("display", "none");
-  				$this.find(".loading-overlay__spinner").removeClass("hidden");
-  				setTimeout(() => {
-  					$thisOrigin.parents("form").find('[type="submit"]').click();
-  				}, 500);
-  				setTimeout(() => {
-  					$this.find(".loading-overlay").css("display", "none");
-  					$this.find(".loading-overlay__spinner").addClass("hidden");
-  					$this.find(".icon-checkmark").show();
-  				}, 1000);
-  				setTimeout(() => {
-  					$this.find(".icon-checkmark").hide();
-  					$this.find(".size_variant_button_add").show();
-  					closeModal(modal, modalOverlay);
-  				}, 2000);
-        }
-      })
-	$("body").on("click", ".variant_modal__toggle_button, .variant_modal__toggle_button svg", openModal);
+	$("body")
+		.on("click", ".variant_selector.submit_on_click .size_variant_button:not(.disabled-variant-button)", function (e) {
+			let modal = $(".variant_modal") || $(".variant_selector");
+			let modalOverlay = $(".variant_modal_overlay");
+			$(this).find(".size_variant_button_add").hide();
+			$(this).find(".loading-overlay").css("display", "flex");
+			$(this).siblings().find(".loading-overlay").css("display", "none");
+			$(this).find(".loading-overlay__spinner").removeClass("hidden");
+			let curRadio = $(this).find('[type="radio"]');
+			curRadio?.prop("checked", true);
+			setTimeout(() => {
+				$(this).parents("form").find('[type="submit"]').click();
+			}, 500);
+			setTimeout(() => {
+				$(this).find(".loading-overlay").css("display", "none");
+				$(this).find(".loading-overlay__spinner").addClass("hidden");
+				$(this).find(".icon-checkmark").show();
+			}, 1000);
+			setTimeout(() => {
+				$(this).find(".icon-checkmark").hide();
+				$(this).find(".size_variant_button_add").show();
+				closeModal(modal, modalOverlay);
+			}, 2000);
+		})
+		.on("click", ".variant_modal__toggle_button, .variant_modal__toggle_button svg", openModal);
 	$("cart-drawer .card__inner").click(openModal);
 
 	function openModal(e) {
@@ -65,13 +59,13 @@ window.addEventListener('DOMContentLoaded', function(event) {
 		modal.show();
 		setTimeout(() => {
 			modal.addClass("active");
-			modal.parents("section").addClass('high-z-index');
+			modal.parents("section").addClass("high-z-index");
 		}, 10);
 		modalOverlay.show();
 		modalOverlay.on("click", () => {
 			closeModal(modal, modalOverlay);
-		})
-		modal.find('.quick_add_modal__close').on("click", () => {
+		});
+		modal.find(".quick_add_modal__close").on("click", () => {
 			closeModal(modal, modalOverlay);
 		});
 	}
